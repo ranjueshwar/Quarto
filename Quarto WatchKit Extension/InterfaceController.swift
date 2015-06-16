8f5f44d041f0689ef50468e8ae71926290a61bb3
Prathap Murthy <prathapmurthy@Prathaps-MacBook-Pro.local>
2015-06-16 13:43:34 +0530

    New Project Committing

---
+++ Quarto WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Quarto WatchKit Extension
+//
+//  Created by Prathap Murthy on 16/06/15.
+//  Copyright (c) 2015 Prathap Murthy. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
